5604764c1a2159e5567019f4523e26dacb60dd24
Ephraim Park <ephiepark@gmail.com>
2022-09-18 19:15:16 -0700

    Firebase Initial Set up

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCo5PX4eewyzK1Ecca_hHLaMuQ3smEKt-w",
+  authDomain: "my-login-app-bc956.firebaseapp.com",
+  projectId: "my-login-app-bc956",
+  storageBucket: "my-login-app-bc956.appspot.com",
+  messagingSenderId: "288290421449",
+  appId: "1:288290421449:web:aefd2bc52a291c0fd22b18",
+  measurementId: "G-RVDTTPMFS0"
+};
