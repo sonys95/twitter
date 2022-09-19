23fbfec576a6e1cd898ccc86ca2bfcb1a5fb3fa4
Ephraim Park <ephiepark@gmail.com>
2022-09-18 20:30:35 -0700

    Add Log In Log Out UI Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import Header from './components/Header';
+import { WithFirebaseApiProps, withFirebaseApi } from './Firebase';
+import { useAppDispatch } from './redux/hooks';
+import { setUserId } from './redux/userSlice';
 
-function App() {
+const App = (props: WithFirebaseApiProps) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    return props.firebaseApi.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(setUserId(user.uid));
+      } else {
+        dispatch(setUserId(null));
+      }
+    });
+  }, []);
+
   return (
     <>
+      <Header />
     </>
   );
 }
 
-export default App;
+export default withFirebaseApi(App);
