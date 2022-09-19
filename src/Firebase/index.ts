406fb7671913e31ce848dac57eaafb96cc179db0
Ephraim Park <ephiepark@gmail.com>
2022-09-18 19:33:50 -0700

    Integrate Firebase Provider

---
+++ src/Firebase/index.ts
@@ -0,0 +1,10 @@
+import FirebaseContext, { withFirebaseApi, WithFirebaseApiProps } from "./firebaseContext";
+import FirebaseApi from "./firebaseApi";
+
+export {
+  FirebaseApi,
+  FirebaseContext,
+  withFirebaseApi
+};
+
+export type { WithFirebaseApiProps };
