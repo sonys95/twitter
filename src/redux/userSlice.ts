9d703bf7854229032830b9742dbe47393130f967
Ephraim Park <ephiepark@gmail.com>
2022-09-18 20:13:32 -0700

    Add User slice Redux store

---
+++ src/redux/userSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface UserState {
+  userId: string | null | undefined;
+}
+
+const initialState: UserState = {
+  userId: undefined,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUserId: (state, action: PayloadAction<string | null>) => {
+      state.userId = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+  },
+});
+
+export const { setUserId } = userSlice.actions;
+
+export default userSlice.reducer;
