cb6a1bba1679439c4b8fd48e952642a7aae3990e
Ephraim Park <ephiepark@gmail.com>
2022-09-18 20:39:29 -0700

    Handle Firebase Loading State

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { CircularProgress } from '@mui/material';
 import { useEffect } from 'react';
 import Header from './components/Header';
 import { WithFirebaseApiProps, withFirebaseApi } from './Firebase';
-import { useAppDispatch } from './redux/hooks';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
+import { RootState } from './redux/store';
 import { setUserId } from './redux/userSlice';
 
+const isLoadingState = (state: RootState): boolean => {
+  return state.user.userId === undefined;
+};
+
 const App = (props: WithFirebaseApiProps) => {
+  const isLoading = useAppSelector(isLoadingState);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -17,6 +24,9 @@ const App = (props: WithFirebaseApiProps) => {
     });
   }, []);
 
+  if (isLoading) {
+    return <CircularProgress sx={{ margin: "auto" }} />;
+  }
   return (
     <>
       <Header />
